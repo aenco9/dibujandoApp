5a14e7c875cc637d3ceb1d52f321ee72d2c0c498
luisferro2 <a01378248@itesm.mx>
2021-10-07 17:28:51 -0500

    Definir estructura de clase de kotlin del fragmento de donaciones.
    Usar el binding.
    Creacion de configurar botones.

--- app/src/main/java/mx/itesm/dibujandoapp/donaciones/FragmentDonaciones.kt
+++ app/src/main/java/mx/itesm/dibujandoapp/donaciones/FragmentDonaciones.kt
@@ -5,56 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import mx.itesm.dibujandoapp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import mx.itesm.dibujandoapp.databinding.FragmentDonacionesBinding
 
 /**
- * A simple [Fragment] subclass.
- * Use the [FragmentDonaciones.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FragmentDonaciones : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+ *
+ * Autor:
+ * Luis Ignacio Ferro Salinas
+ * Última modificación:
+ * 7 de octubre de 2021
+ *
+ * */
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class FragmentDonaciones : Fragment() {
+
+
+    // This is the reference to the view.
+    private lateinit var binding: FragmentDonacionesBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donaciones, container, false)
+        // The binding contains the view that this onCreateView function returns.
+        binding = FragmentDonacionesBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentDonaciones.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentDonaciones().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        respondToTaps()
+    }
+
+    private fun respondToTaps() {
+        TODO("oh boi")
+        /*
+        val myAction = FragmentDonacionesDirections.actionScrollingFragmentToDatosDonaciones()
+        binding.campaignImageViewBtn1.setOnClickListener {
+            findNavController().navigate(myAction)
+        }
+        binding.campaignImageViewBtn2.setOnClickListener {
+            findNavController().navigate(myAction)
+        }
+        binding.payPalDonateOnceBtn.setOnClickListener {
+            findNavController().navigate(myAction)
+
+        }
+        binding.payPalDonateMonthlyBtn.setOnClickListener {
+            findNavController().navigate(myAction)
+
+        }
+        binding.creditCardDonateBtn.setOnClickListener {
+            if (binding.montoCreditCardEditTextDecimal.text.isEmpty()) {
+                Toast.makeText(getActivity(), "Por favor introduzca un monto",
+                    Toast.LENGTH_LONG).show();
+            } else {
+                findNavController().navigate(myAction)
             }
+        }
+        binding.payPalAddBtn.setOnClickListener {
+            binding.montoPaypalEditTextDecimal.setText("MORE")
+        }
+        binding.payPalSubstractBtn.setOnClickListener {
+            binding.montoPaypalEditTextDecimal.setText("LESS")
+        }
+
+         */
     }
 }
