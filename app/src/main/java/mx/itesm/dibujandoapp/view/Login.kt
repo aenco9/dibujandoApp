983536a75e34102c456c30097018ad05fa7df41f
AlexisCastaneda <79714949+AlexisCastaneda@users.noreply.github.com>
2021-10-18 10:46:05 -0500

    LogIn con Google
    Parte1 para hacer el LogIn con google

--- app/src/main/java/mx/itesm/dibujandoapp/view/Login.kt
+++ app/src/main/java/mx/itesm/dibujandoapp/view/Login.kt
@@ -1,12 +1,18 @@
 package mx.itesm.dibujandoapp.view
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import mx.itesm.dibujandoapp.R
 import mx.itesm.dibujandoapp.databinding.LoginFragmentBinding
 import mx.itesm.dibujandoapp.viewmodel.LoginVM
@@ -16,6 +22,8 @@ class login : Fragment() {
     companion object {
         fun newInstance() = login()
     }
+
+    private val CODIGO_SIGNIN: Int = 500
 
     private lateinit var viewModel: LoginVM
 
@@ -27,7 +35,59 @@ class login : Fragment() {
     ): View? {
         binding= LoginFragmentBinding.inflate(layoutInflater)
         val vista= binding.root
+        configurarEvento()
         return vista
+    }
+
+    private fun configurarEvento() {
+        binding.btnRegistrar2.setOnClickListener {
+            autenticar()
+        }
+        binding.btnRegistar.setOnClickListener {
+            autenticar()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CODIGO_SIGNIN) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    val usuario = FirebaseAuth.getInstance().currentUser
+                    FirebaseAuth.getInstance().currentUser
+                    println("Bienvenido: ${usuario?.displayName}")
+                    println("Correo: ${usuario?.email}")
+                    println("Correo: ${usuario?.uid}")
+                    // Lanzar otra actividad
+                }
+                AppCompatActivity.RESULT_CANCELED -> {
+                    println("Cancelado...")
+                    val response =
+                        IdpResponse.fromResultIntent(data)
+                    println("Error:${response?.error?.localizedMessage}")
+                }
+                else -> {
+                    val response =
+                        IdpResponse.fromResultIntent(data)
+                    println("Error:${response?.error?.errorCode}")
+                }
+            }
+        }
+
+    }
+
+    private fun autenticar() {
+        val providers =
+            arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            CODIGO_SIGNIN
+        )
     }
 
 
@@ -35,9 +95,9 @@ class login : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //Eventos
 
-        binding.btnRegistar.setOnClickListener {
+       /* binding.btnRegistar.setOnClickListener {
             findNavController().navigate(R.id.action_login_to_pantallaRegistro)
-        }
+        }*/
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
