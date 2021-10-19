dc815657d9a5b9e5c965df7c99c4773cb7a67a8e
luisferro2 <a01378248@itesm.mx>
2021-10-19 14:06:44 -0500

    Añadir el logo de la aplicación

--- app/src/main/java/mx/itesm/dibujandoapp/donaciones/PayPalConfirmation.kt
+++ app/src/main/java/mx/itesm/dibujandoapp/donaciones/PayPalConfirmation.kt
@@ -1,11 +1,13 @@
 package mx.itesm.dibujandoapp.donaciones
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import mx.itesm.dibujandoapp.PrincipalActivity
 import mx.itesm.dibujandoapp.databinding.FragmentPayPalConfirmationBinding
 
 /**
@@ -30,6 +32,10 @@ class PayPalConfirmation : Fragment() {
     ): View? {
         binding = FragmentPayPalConfirmationBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         respondToConfirmButtonTap()
     }
 
